391df75f112a21109fda70c70a330046cddf6d9a
Ralf Gueldemeister <ralf@gueldemeister.de>
2015-05-23 14:19:48 -0700

    Add getAssignableGroups request and initial board template

--- app.js
+++ app.js
@@ -5,15 +5,32 @@
     requests: require('requests.js'),
 
     tickets: [],
+    groups: [],
 
     setTickets: function(data) {
       this.tickets = data.rows.map(function(row) {
         return row.ticket;
       });
+      this.trigger('reloadBoard');
+    },
+
+    setGroups: function(data) {
+      this.groups = data.groups;
+      this.trigger('reloadBoard');
+    },
+
+    reloadBoard: function() {
+      var context = {
+        tickets: this.tickets,
+        groups: this.groups
+      };
+
+      this.switchTo('board', context);
     },
 
     appActivated: function() {
-      this.ajax('previewTicketView').done(this.setTickets);
+      this.ajax('previewTicketView');
+      this.ajax('getAssignableGroups');
     }
   };
 
