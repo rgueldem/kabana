add130167fad7b046f8f805bc5b3948326e4bfd7
Travers McInerney <travers@mcinerney.me>
2015-05-23 18:53:53 -0700

    Add initial board layout

--- app.js
+++ app.js
@@ -7,37 +7,56 @@
   };
 
   return {
+    data: {
+      groups: [],
+      tickets: [],
+      statuses: [ 'new', 'open', 'pending', 'hold', 'solved', 'closed' ]
+    },
+    draggedEl: null,
     events: require('events.js'),
     requests: require('requests.js'),
 
-    tickets: [],
-    groups: [],
-    draggedEl: null,
+    getTicketStatusTranslation: function(value) {
+      return {
+        'title': this.I18n.t('ticket_statuses.' + value),
+        'value': value
+      };
+    },
 
     setTickets: function(data) {
-      this.tickets = data.rows.map(function(row) {
+      var ticketsGroupedByStatus = [];
+      this.data.tickets = data.rows.map(function(row) {
         return _.extend(row.ticket, {
           assignee: findById(data.users, row.assignee_id)
         });
       });
+
+      // Associate tickets with statuses
+      ticketsGroupedByStatus = _.groupBy(this.data.tickets, 'status');
+      this.data.statuses     = this.data.statuses.map(function(status) {
+        var tickets = ticketsGroupedByStatus[status.value] || [];
+        return _.extend(status, {
+          hasTickets: tickets.length > 0,
+          tickets: tickets
+        });
+      });
+
       this.trigger('reloadBoard');
     },
 
     setGroups: function(data) {
-      this.groups = data.groups;
+      this.data.groups = data.groups;
       this.trigger('reloadBoard');
     },
 
     reloadBoard: function() {
-      var context = {
-        tickets: this.tickets,
-        groups: this.groups
-      };
-
-      this.switchTo('board', context);
+      this.switchTo('board', this.data);
     },
 
     appActivated: function() {
+      // Load translations for ticket statuses
+      this.data.statuses = this.data.statuses.map(this.getTicketStatusTranslation.bind(this));
+
       this.ajax('previewTicketView');
       this.ajax('getAssignableGroups');
     },
@@ -49,7 +68,7 @@
     dropTicket: function(e) {
       e.preventDefault();
 
-      var dropzone = this.$(e.target).closest('.dropzone'),
+      var dropzone = this.$(e.target).closest('.ticket-status'),
           id = this.draggedEl.data('id'),
           newStatus = dropzone.data('status');
 
@@ -66,7 +85,7 @@
     transitionTicket: function(e) {
       var ticket, change = {};
 
-      ticket = findById(this.tickets, e.id);
+      ticket = findById(this.data.tickets, e.id);
 
       if (ticket === undefined || ticket.status === e.newStatus) {
         return;
